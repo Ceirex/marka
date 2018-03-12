3157d730bf824c6534bf0212e679cc2c62a39fe1
pbp2996 <pbp2996@yahoo.com>
2018-03-12 19:38:13 -0400

    added user_tags
    - modify user menu
    	- add partition for advertisments
    
    - create view tags
          - with thumbnails and qr img

---
+++ user/user_tags.php
@@ -0,0 +1,38 @@
+<?php include '../common/configuration.php';?>
+<?php include '../view/header.php';?>
+
+<div class="part8" style="background-color: #eee; height: 650px;padding-top: 75px">
+
+	<div class="container" style="border-radius: 15px; padding: 50px;background-color: #fff;">
+
+		<div class="row">
+			<div class="col-sm-6 col-md-4 col-lg-3">
+			    <div class="thumbnail">
+		            <img src="https://blog.brother.es/wp-content/uploads/2013/03/Qr-Brother.png" alt="ocnj_seasonal">
+					<div class="caption">
+						<center><h3>Cap May</h3></center>
+						<p style="color: black"><i>Seasonal</i></p>
+						<p><a href="#" class="btn btn-primary" role="button">View</a>  <a href="#" class="btn btn-danger" role="button">Remove</a></p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 col-md-4 col-lg-3">
+				<div class="thumbnail">
+		           	<img src="https://blog.brother.es/wp-content/uploads/2013/03/Qr-Brother.png" alt="ocnj_Weekly">
+				    <div class="caption">
+					    <center><h3>Wildwood</h3></center>
+					    <p style="color: black"><i>Weekly</i></p>
+					    <p><a href="#" class="btn btn-primary" role="button">View</a>  <a href="#" class="btn btn-danger" role="button">Remove</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+	</div>
+	
+</div>
+
+
+<?php include '../view/footer.php';?>
