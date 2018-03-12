87c885b01b6b930da2f3a681c35f63d30d49edbb
Sean4Doyle <36742233+Sean4Doyle@users.noreply.github.com>
2018-03-12 14:12:41 -0400

    Add files via upload

---
+++ advertisement/ad_list.php
@@ -0,0 +1,73 @@
+<?php include '../common/configuration.php';?>
+<?php include '../view/header.php';?>
+
+<div id="beach_ads">
+    <div class="gallery">
+      <a target="_blank" href="../images/fireworks.jpg">
+        <img src="../images/fireworks.jpg" alt="fireworks" width="300" height="200">
+      </a>
+      <div class="desc">Watch the fire works with us on the 4th of July</div>
+    </div>
+
+    <div class="gallery">
+      <a target="_blank" href="../images/parade.jpg">
+        <img src=../images/"parade.jpg" alt="parade" width="300" height="200">
+      </a>
+      <div class="desc">June 12th is national parade day, come watch</div>
+    </div>
+
+    <div class="gallery">
+      <a target="_blank" href="../images/surfing.jpg">
+        <img src="../images/surfing.jpg" alt="surfing" width="300" height="200">
+      </a>
+      <div class="desc">Free surfing lesson at 9th street from 9:00-10:00am</div>
+    </div>
+
+    <div class="gallery">
+      <a target="_blank" href="../images/volleyball.jpg">
+          <img src="../images/volleyball.jpg" alt="volleyball" width="300" height="200">
+      </a>
+      <div class="desc">Summer slam volleyball registration opens Augest 3rd at 4:00pm</div>
+    </div>
+</div>
+    
+    &nbsp;
+    <hr>
+    &nbsp;
+<!--    
+<div id="beach_ads">
+    <div class="gallery">
+      <a target="_blank" href="../images/fireworks.jpg">
+        <img src="../images/fireworks.jpg" alt="fireworks" width="300" height="200">
+      </a>
+      <div class="desc">Watch the fire works with us on the 4th of July</div>
+    </div>
+
+    <div class="gallery">
+      <a target="_blank" href="../images/parade.jpg">
+        <img src=../images/"parade.jpg" alt="parade" width="300" height="200">
+      </a>
+      <div class="desc">June 12th is national parade day, come watch</div>
+    </div>
+
+    <div class="gallery">
+      <a target="_blank" href="../images/surfing.jpg">
+        <img src="../images/surfing.jpg" alt="surfing" width="300" height="200">
+      </a>
+      <div class="desc">Free surfing lesson at 9th street from 9:00-10:00am</div>
+    </div>
+
+    <div class="gallery">
+      <a target="_blank" href="../images/volleyball.jpg">
+          <img src="../images/volleyball.jpg" alt="volleyball" width="300" height="200">
+      </a>
+      <div class="desc">Summer slam volleyball registration opens Augest 3rd at 4:00pm</div>
+    </div>
+</div>
+-->
+
+</body>
+
+</html>
+
+<?php include '../view/footer.php';?>
---
+++ view/footer.php
@@ -0,0 +1,57 @@
+
+<footer>
+    <div class="footer" id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                    <h3>More Info</h3>
+                    <ul>
+                        <li> <a href="#">About Us | Contact</a> </li>
+                        <li> <a href="#">Careers</a> </li>
+                        <li> <a href="#">Terms of Use</a> </li>
+                    </ul>
+                </div>
+                <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                    <h3>Advertising</h3>
+                    <ul>
+                        <li> <a href="#">Advertising Info</a> </li>
+                        <li> <a href="#">Media Planner</a> </li>
+                        <li> <a href="#">Digital Specs & FTP Info</a> </li>
+                    </ul>
+                </div>
+                
+                <div class="col-lg-4  col-md-3 col-sm-6 col-xs-12 " style="float: right;">
+                    <h3> Sign Up</h3>
+                    <ul>
+						<form>
+						    <label for="emailinput">Email address:</label><br>
+
+							<div class="input-group">
+							    <input type="email" id="emailinput" class="form-control width100" required>
+							    <span class="input-group-btn">
+							        <button class="btn btn-success">Submit</button>
+							    </span>
+							</div>
+						</form>
+                    </ul>
+                    <ul class="social">
+                        <li><a href="#"><img src="<?php echo $base_path . '/images/facebook.png'?>"></a></li>
+                        <li><a href="#"><img src="<?php echo $base_path . '/images/instagram.png'?>"></a></li>
+                        <li><a href="#"><img src="<?php echo $base_path . '/images/pinterest.png'?>"></a></li>
+                    </ul>
+                </div>
+            </div>
+            <!--/.row-->             
+            <br><br><p class="pull-left"><center> Copyright © Marka. All right reserved.</center></p>
+
+        </div>
+        <!--/.container--> 
+    </div>
+    <!--/.footer-->
+    
+    <!--/.footer-bottom--> 
+</footer>
+	
+    </main>
+</body>
+</html>
