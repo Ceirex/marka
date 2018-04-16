1bc889c17e00b300ccf0e759ee1e0abef79b04d9
Sean4Doyle <36742233+Sean4Doyle@users.noreply.github.com>
2018-04-16 13:58:55 -0400

    Fix
    Fixed weird errors

--- common/configuration.php
+++ common/configuration.php
@@ -1,8 +1,8 @@
 <?php
     //this is for the database
     $dsn = 'mysql:host=localhost;dbname=marka_db';
-    $username = 'root';
-    $password = 'password2996';
+    $username = 'marka_db';
+    $password = 'password';
     
     //this path can be used in place of relative references in HTML
     $base_path = 'http://localhost/marka';
