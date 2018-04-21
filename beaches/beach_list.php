837f593e5bb8d0ea225e10e1d7b97d342f4a5b77
pbp2996 <pbp2996@yahoo.com>
2018-04-21 14:26:37 -0400

    addded icons to beach_list

--- beaches/beach_list.php
+++ beaches/beach_list.php
@@ -32,7 +32,11 @@ $beaches = getBeaches();
 					   id: '" . $beach['beach_id'] . "',
 					   img: '" . $beach['beach_img'] ."',
 					   loc: '" . $beach['beach_location'] ."',
-					   desc: '" . $beach['beach_desc'] ."'
+					   desc: '" . $beach['beach_desc'] ."', 
+					   beachact: '" . $beach['beach_active'] . "',
+					   beachfam: '" . $beach['beach_family'] . "',
+					   beachdog: '" . $beach['beach_dog'] . "',
+					   beachpark: '" . $beach['beach_parking'] . "'
 					  },";
 			} 
 		?> 
@@ -53,7 +57,41 @@ $beaches = getBeaches();
 					<div class="col-lg-9 col-md-9 col-sm-12" style="padding-left: 0">
 						<h4 style="color: black; font-size: 1.1em"><b>Location:</b> {{beach.loc}}</h4><br>
 						<p style="color: black; text-align: justify;font-size: .95em"><b>Why it's awesome:</b> {{beach.desc}}</p>
-					</div>						<a href="<?php echo $base_path . '/tags?beachid={{beach.id}}&userid='. $user_id ;?>"><button type="button" class="btn btn-success btn-md" style="margin-top: 20px;">Choose Beach</button></a>
+					</div>						
+
+						<a href="<?php echo $base_path . '/tags?beachid={{beach.id}}&userid='. $user_id ;?>">
+							<button type="button" class="btn btn-success btn-md" style="margin-top: 20px;">Choose Beach </button>
+						</a>
+					
+					<span style="padding-left: 20px; display: inline-block;">
+						<?php 
+						$beachact = '{{beach.beachact}}';
+						$beachfam = '{{beach.beachfam}}';
+						$beachdog = '{{beach.beachdog}}';
+						$beachpark = '{{beach.beachpark}}';
+
+				
+						if (!empty($beachact)){
+							echo '<img style="padding-top: 10px; padding-left: 10px" src="' . $base_path . '/images/{{beach.beachact}}" height="60" width="60" alt="" title="Activities friendly Beach">';
+						}
+
+						if (!empty($beachfam)){
+							echo '<img style="padding-top: 10px;padding-left: 10px" src="' . $base_path . '/images/{{beach.beachfam}}" height="60" width="60" alt="" title="Family friendly beach">';
+						}
+
+						if (!empty($beachdog)){
+							echo '<img style="padding-top: 10px;padding-left: 10px" src="' . $base_path . '/images/{{beach.beachdog}}" height="60" width="60" alt="" title="Pet friendly beach">';
+						}						
+
+						if (!empty($beachpark)){
+							echo '<img style="padding-top: 10px;padding-left: 10px" src="' . $base_path . '/images/{{beach.beachpark}}" height="60" width="60" alt="" title="Parking friendly beach">';
+						}
+
+						?>
+						
+
+					</span>
+						
 
 				</div><hr>
 
--- planyourtrip/activities.php
+++ planyourtrip/activities.php
@@ -2,6 +2,39 @@
 
 <div class="part9">
 
+	<div class="jumbotron jumbotron-fluid" style="background-color: #eee;margin-bottom: 0px;">
+	  <div class="container" style="background-color: rgba(0,0,0,.1);padding: 30px;border-radius: 30px;">
+		<h1 style="color: #05386B">Activities</h1>    
+			<div class="resp" style="clear: both;padding: 25px 10px 20px 10px">
+				    <p class="lead" style="color: #05386B">Add some more fun to your visit. Browse the awesome activities, learn more and get participating now. Make your next beach visist awesome.</p><br>
+				  </div>	
+			</div>
+	</div>
+
+	<div class="container">
+		<div id="img_grid" style="background-color: #e7e7e7;height: 700px;overflow: scroll;padding-top: 40px;">
+
+
+			<div class="row">
+				<div class="column">
+					<img src="http://www.georgekarbusphotography.com/wp-content/uploads/2015/08/fergal_smith_surfing_black_and_white.jpg" height="200" width="" alt="400placeholder+image">
+				</div>
+
+				<div class="column">
+					<img src="http://www.georgekarbusphotography.com/wp-content/uploads/2015/08/fergal_smith_surfing_black_and_white.jpg" height="200" width="300" alt="placeholder+image">
+				</div>
+
+				<div class="column">
+					<img src="http://www.georgekarbusphotography.com/wp-content/uploads/2015/08/fergal_smith_surfing_black_and_white.jpg" height="200" width="300" alt="placeholder+image">
+				</div>
+
+			</div>
+
+
+
+
+		</div>
+	</div>
 
 </div>
 
